e8f2aaee12614c70f2d228a0fe5457a32f3f308a
lucasgrzegorczyk <lucasgrzegorczyk@gmail.com>
2014-02-02 15:32:46 +0100

    Test commit #4.

--- src/pl/furai/learningandroid/MainActivity.java
+++ src/pl/furai/learningandroid/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-//test #3
+//test #4
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {

169927d4373bb38b19acb8ae19cc4a2df0ffe6e7
Furai <furai@thd.vg>
2014-02-05 00:59:46 +0100

    [added] Stub functions for action bar items

--- src/pl/furai/learningandroid/MainActivity.java
+++ src/pl/furai/learningandroid/MainActivity.java
@@ -4,13 +4,15 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
 	/**
-	 * String used for the key of extra message sent via Intent to the DisplayMessageActivity class.
+	 * String used for the key of extra message sent via Intent to the
+	 * DisplayMessageActivity class.
 	 */
 	public final static String EXTRA_MESSAGE = "pl.furai.learningandroid.MESSAGE";
 
@@ -25,6 +27,22 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main_activity_actions, menu);
 		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle presses on the action bar items
+		switch (item.getItemId()) {
+			case R.id.action_search:
+				//openSearch();
+				return true;
+			case R.id.action_settings:
+				//openSettings();
+				return true;
+			default:
+				return super.onOptionsItemSelected(item);
+		}
+		
 	}
 
 	/**
